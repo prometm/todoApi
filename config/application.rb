6166b533dd6dc5e32cbba017059ad6c59b699cd2
prometm <prometm@i.ua>
2020-09-19 14:07:26 +0300

    New allowed host

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module Api
     # CORS
     config.middleware.use Rack::Cors do
       allow do
-        origins '*'
+        origins 'http://maks-ui-todo.herokuapp.com/'
         resource '*',
           headers: :any,
           expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
